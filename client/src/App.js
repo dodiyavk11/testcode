9e14958181d15b30ad1ebeeb9e09c10d7b3f0db4
besticoder <besticoder@gmail.com>
2021-11-27 05:38:24 +0530

    test done

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,116 @@
 import './App.css'
-import React, { setSelected, useState } from "react";
+import axios from 'axios';
+import React, { useState } from 'react';
 
-// Defining our App Component
-const App = () => {
+const App = (props) => {
+  const [select, selected] = useState(null); // state for storing actual image
+  const [state, setState] = useState(null);
+  const [data, setData] = useState([]);
 
-  const [selected, setSelected] = useState({});
-   
-  const  tab1Click = (tabId = null) => {
-    setSelected({ ...selected, [tabId]: !selected[tabId] })
-  } 
-     
-  // Returning our JSX code
-  return <>
+  const handleInputChange = (event) => {
+    setState(event.target.files[0]);
+  };
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", state);
+    console.log(state);
+    if(state.name.includes(".csv"))
+    {
+    axios.post("http://localhost:5000/product/add", formData)
+      .then(res => {
+        if(res.data.error){
+          alert(res.data.error);
+        }else{
+          alert('file data uploaded successfully');
+          tab2Click();
+        }
+      })
+      .catch(function (error) {
+        alert(error);
+      })
+    }
+    else
+    {
+      alert('please upload csv file');
+    }
+  };
+
+  const tab1Click = async (event) => {
+    selected(1)
+  };
+
+  const tab2Click = async (event) => {
+    selected(2)
+    var self = this;
+    axios.post("http://localhost:5000/product/getProducts", {})
+      .then(res => {
+        if(res.data.lenth==0)
+        {
+          alert('no data found');
+        }
+        else
+        {
+          setData(res.data);
+        }
+        
+      })
+  };
+
+  return (
     <div className="App">
       <header className="App-header">
         <div className="header">Demosite</div>
-        <div className="Validate"><button className="validatebtn"> Validate </button></div>
+        <div className="Validate">
+          <button className="validatebtn"> Validate</button>
+        </div>
         <div className="tabs">
           <ul className="product">
-            <li onClick={tab1Click[1]} className={selected == 1 ? "tab Active" : "tab"}>
+            <li onClick={tab1Click} className={select === 1 ||  !select ? "tab Active" : "tab"}>
               Product
             </li>
-            <li onClick={tab1Click[2]} className={selected == 2 ? "tab Active" : "tab"}>
+            <li onClick={tab2Click} className={select === 2 ? "tab Active" : "tab"}>
               Result
             </li>
           </ul>
           <div className="tabsview">
-            <div className="productview activeview">
+            <div className={select === 1 ||  !select ? "resultview activeview" : "resultview"}>
               <h1>Upload CSV</h1>
-              <form enctype="multipart/form-data">
-                <input type='file'  />
-                <button > Upload </button>
+              <form onSubmit={handleOnSubmit}>
+                <input type='file' name='file' onChange={handleInputChange} />
+                <button> Upload</button>
               </form>
             </div>
+            <div className={select === 2 ? "resultview activeview" : "resultview"}>
+            <table width="100%" border="1px">
+              <tr>
+                <th>Id</th>
+                <th>name</th>
+                <th>Dimansions</th>
+                <th>Image</th>
+              </tr>
+              {data.map((pro, index) => (
+                <tr data-index={index}>
+                  <td style={{color: pro.id === "NA" ? "red" : ""}}>{pro.id}</td>
+                  <td style={{color: pro.name === "NA" ? "red" : ""}}>{pro.name}</td>
+                  <td style={{color: pro.picture.image === "NA" ? "red" : ""}}>{pro.picture.width} * {pro.picture.height}</td>
+                  <td style={{color: pro.picture.image === "NA" ? "red" : ""}}>
+                    {pro.picture.image != 'NA' ? (
+                      <img src={pro.picture.image} width="100" />
+                    ) : (
+                      'NA'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </table>
           </div>
-          <div className="resultview">abcd</div>
+          </div>
         </div>
       </header>
     </div>
-  </>;
-}
-export default App
+  );
+};
+
+export default App;
--- server/product.js
+++ server/product.js
@@ -1,42 +1,139 @@
 import express from "express";
+const fileUpload = require('express-fileupload');
 const router = express.Router();
 const fs = require("fs");
 const csv = require("fast-csv");
 
+router.use(fileUpload());
 
-router.post('/add', upload.single("file"), (req, res) => {
+
+router.post('/getProducts', (req, res) => {
+
+  let dataPath = __dirname + '/data/images.json';
+  var fs = require('fs');
+  var json;
+  fs.readFile(dataPath, 'utf8', function (err, data) {
+    if (err) throw err;
+    json = JSON.parse(data);
+    res.json(json);
+  });
+});
+
+router.post('/add', async (req, res) => {
     try {
-        if (req.file == undefined) {
+        var fs = require('fs');
+        let dataPath = __dirname + '/data/images.json';
+        if(!req.files) {
           return res.status(400).send("Please upload a CSV file!");
         }
+        var error1 = 0;
+        var i = 0;
+        let sampleFile = req.files.file;
+        var json = await getFiles(sampleFile);
+        var data = await getFiles1(sampleFile);
 
-        let tutorials = [];
-        let path = __basedir + "/uploads/" + req.file.file;
+        json = json.olddata;
+        data = data.newdata;
+        var mydata = [];
 
-        fs.createReadStream(path)
-          .pipe(csv.parse({ headers: true }))
-          .on("error", (error) => {
-            throw error.message;
-          })
-          .on("data", (row) => {
-            dataList.push(row);
-          })
-          .on("end", () => {
-            console.log(dataList);
-          });
-      } catch (error) {
-        console.log(error);
-        res.status(500).send({
-          message: "Could not upload the file: " + req.file.originalname,
+        var dataArray = data.split(/\r?\n/);  //Be careful if you are in a \r\n world...
+        dataArray.forEach(function (item) {
+          const myArray = item.split(";");
+
+          var id = 'NA';
+          if(myArray[0]){
+            id = myArray[0].replace('"', '');
+          }
+          var name = 'NA';
+          if(myArray[1]){
+            name =myArray[1].replace('"', '');
+          }
+          var image = 'NA';
+          if(myArray[2]){
+            image =myArray[2].replace('"', '');
+          }
+          var width = 2048;
+          var height = 1536;
+          if(image == 'NA'){
+            width = 'NA';
+            height = 'NA';
+          }
+
+          if(i==0){
+            if( id !== 'id"' || name !== 'name"' || image !== 'url"')
+            {
+              error1 = 1;
+            }
+          }
+          if(i!=0){
+            if( id === 'NA' && name === 'NA' && image === 'NA'){
+
+            }else{
+              var newarray = {
+                id:id.replace('"', ''),
+                name:name.replace('"', ''),
+                picture:{
+                  image:image.replace('"', ''),
+                  width:width,
+                  height: height
+                }
+              }
+              mydata.push(newarray);
+            }
+          }
+          i = 1;
+        });
+      var lastdata = json.concat(mydata);
+      fs.writeFile(dataPath,JSON.stringify(lastdata),function(err){
+        if(err) throw err;
+      })
+
+      console.log(error1);
+      if(error1 == 0){
+        res.json(true);
+      }else{
+        res.status(200).send({
+          data: [],
+          error: 'CSV File dose not contain valit columns'
         });
       }
-    res.json('ok');
+
+
+    } catch (error) {
+        res.status(500).send({
+          message: "Could not upload the file: ",
+        });
+    }
 });
 
+async function getFiles(sampleFile) {
+  return new Promise((res, rej) => {
+    let dataPath = __dirname + '/data/images.json';
+    var fs = require('fs');
 
-router.get('/get', (req, res) => {
-    
-});
+    fs.readFile(dataPath, 'utf8', function (err, data) {
+      if (err) rej(err);
+      res({ olddata: data });
+    });
+    res({ olddata: [] });
+  });
+  return olddata;
+}
 
+async function getFiles1(sampleFile) {
+  return new Promise((res, rej) => {
+    let uploadPath = __dirname + '/uploads/import' + sampleFile.name;
+    sampleFile.mv(uploadPath);
+
+
+    var fs = require('fs');
+
+    fs.readFile(uploadPath, 'utf8', function (err, data) {
+      if (err) rej(err);
+      res({ newdata: data });
+    });
+  });
+  return newdata;
+}
 
 module.exports = router;
